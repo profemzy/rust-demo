6eae92423d158f2f5fea0cee784d8ecf71519e22
Femi Oladele <femioladele@infotitans.com>
2023-10-28 12:33:49 -0700

    Using loops

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-  println!("Hello World Rust")
+  let arr = [ 99, 55, 95, 100, 82 ];
+
+  for elem in arr {
+    println!("{}", elem)
+  }
+
+  for number in 0..10 {
+    println!("{}", number)
+  }
+
+  for item in 0..=10 {
+    println!("{}", item)
+  }
 }
+
